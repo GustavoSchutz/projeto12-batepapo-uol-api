e6f15e07947e6a13228bd28d81d6698522b0eb55
GustavoSchutz <gutofelipe8@gmail.com>
2022-09-02 17:26:20 -0300

    build: mongodb

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient } from "mongodb";
 
 const app = express();
+
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+let db;
+
 app.use(cors());
+app.use(express.json());
+
+mongoClient.connect().then(() => {
+	db = mongoClient.db("test"); //Alterar para projeto12-batepapo-uol-api
+});
+
+app.listen(5000, () => console.log('Listening on port 5000'));
