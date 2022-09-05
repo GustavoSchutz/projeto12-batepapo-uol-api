73b7dfdc55a287b4ff80cac3f2cad0de4868db2d
GustavoSchutz <gutofelipe8@gmail.com>
2022-09-05 03:32:55 -0300

    build: dayjs

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const app = express();
 const userSchema = joi.object({
 	name: joi.string()
 		.required()
+});
+const messageSchema = joi.object({
+	to: joi.string().required(),
+	text: joi.string().required(),
+	type: joi.string().valid('message', 'private_message').required()
 })
 
 app.use(cors());
@@ -57,7 +62,7 @@ app.get("/participants", async (req, res) => {
 		const participants = await db.collection("participants").find({}).toArray();
 
 		res.send(participants);
-		
+
 	} catch (error) {
 		console.log(error);
 		res.sendStatus(500);
@@ -65,7 +70,12 @@ app.get("/participants", async (req, res) => {
 	
 });
 
+app.post("/messages", async (req, res) => {
+	const message = req.body;
 
+
+
+})
 
 
 
