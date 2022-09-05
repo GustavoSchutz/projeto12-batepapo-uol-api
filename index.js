aaf52769b52de623049b9eda625ee400acce99f6
GustavoSchutz <gutofelipe8@gmail.com>
2022-09-05 02:02:00 -0300

    build: joi, dotenv

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
 
 const app = express();
 
-const mongoClient = new MongoClient("mongodb://localhost:27017");
-let db;
-
 app.use(cors());
 app.use(express.json());
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
 
 mongoClient.connect().then(() => {
 	db = mongoClient.db("test"); //Alterar para projeto12-batepapo-uol-api
-});
+}).catch(() => console.log(error));
+
+
 
 app.listen(5000, () => console.log('Listening on port 5000'));
